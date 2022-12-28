52515cfb924ed7f9fbf978d91af4244903fd408c
Vinicios <114757859+XDuckBlackX@users.noreply.github.com>
2022-12-28 11:47:09 -0300

    Update OpenStack.sh
    Signed-off-by: Vinicios <114757859+XDuckBlackX@users.noreply.github.com>

--- OpenStack.sh
+++ OpenStack.sh
@@ -1,3 +1,7 @@
+#Script para a criação do usuario stack
+#Adicioná-lo ao grupo de administradores
+#E clonar o repositório do devstack mais recente
+
 #!/bin/bash
 
 sudo apt update
@@ -12,23 +16,7 @@ sudo -u stack -i
 
 
 git clone https://opendev.org/openstack/devstack 
-#caso queira puxar uma outra atualização mais antiga usar no final
-#-b stable/#NomeDaAtualização
+#caso queira puxar uma outra atualização mais antiga usar no final "-b stable/#NomeDaAtualização"
 cd devstack
 
-sudo echo '[[local|localrc]]
-ADMIN_PASSWORD=serverEaD
-DATABASE_PASSWORD=$ADMIN_PASSWORD
-RABBIT_PASSWORD=$ADMIN_PASSWORD
-SERVICE_PASSWORD=$ADMIN_PASSWORD
-HOST_IP=172.16.1.103
-FLAT_INTERFACE=enp3s4f0
-FLOATING_RANGE=172.16.1.103/24
-FIXED_RANGE=10.11.12.0/24
-FIXED_NETWORK_SIZE=256
-SWIFT_REPLICAS=1
-
-enable_service s-proxy s-object s-container s-account
-enable_plugin magnum http://opendev.org/openstack/magnum
-enable_plugin magnum-ui http://opendev.org/openstack/magnum-ui' > /opt/stack/devstack/local.conf
-sudo chown stack:stack -R /opt/stack
+###Caso queira fazer por linha de comando cada um dos comando acima também é possivel###
